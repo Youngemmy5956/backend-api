cc94359e15371ea1a86d8f7a7c17485923ef51e2
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-07-07 15:33:26 +0100

    updated items

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,187 +9,191 @@ import Order_model from "../model/orders.js";
 
 const router = express.Router();
 
-
 // get cart
 
-router.get("/cart",   async (req, res) => {
+router.get("/cart", async (req, res) => {
   const owner = req.user._id;
   try {
-      const cart = await Cart_model.findOne({ owner });
-  if (cart && cart.items.length > 0) {
-       res.status(200).send(cart);
-  } else {
-        res.send(null);
-  }
+    const cart = await Cart_model.findOne({ owner });
+    if (cart && cart.items.length > 0) {
+      res.status(200).send(cart);
+    } else {
+      res.send(null);
+    }
   } catch (error) {
-      res.status(500).send();
+    res.status(500).send();
   }
-  });
+});
 
 //   // create cart
 
-  router.post("/cart", async (req, res) => {
-const owner = req.user._id;
-const { itemId, quantity } = req.body;
-try {
+router.post("/cart", async (req, res) => {
+  const owner = req.user._id;
+  const { itemId, quantity } = req.body;
+  try {
     const cart = await Cart_model.findOne({ owner });
     const item = await Item_model.findOne({ _id: itemId });
-if (!item) {
-    res.status(404).send({ message: "item not found" });
-    return;
-}
+    if (!item) {
+      res.status(404).send({ message: "item not found" });
+      return;
+    }
     const price = item.price;
     const name = item.name;
-//If cart already exists for user,
-if (cart) {
-    const itemIndex = cart.items.findIndex((item) => item.itemId ==  itemId);
-//check if product exists or not
-if (itemIndex > -1) {
-    let product = cart.items[itemIndex];
-    product.quantity += quantity;
-    cart.bill = cart.items.reduce((acc, curr) => {
-       return acc + curr.quantity * curr.price;
-   },0)
-cart.items[itemIndex] = product;
-   await cart.save();
-   res.status(200).send(cart);
-} else {
-   cart.items.push({ itemId, name, quantity, price });
-   cart.bill = cart.items.reduce((acc, curr) => {
-   return acc + curr.quantity * curr.price;
-},0)
-   await cart.save();
-   res.status(200).send(cart);
-}
-} else {
-//no cart exists, create one
-const newCart = await Cart_model.create({
-   owner,
-   items: [{ itemId, name, quantity, price }],
-    bill: quantity * price,
-});
-return res.status(201).send(newCart);
-}
-} catch (error) {
-   console.log(error);
-   res.status(500).send("something went wrong");
-}
+    //If cart already exists for user,
+    if (cart) {
+      const itemIndex = cart.items.findIndex((item) => item.itemId == itemId);
+      //check if product exists or not
+      if (itemIndex > -1) {
+        let product = cart.items[itemIndex];
+        product.quantity += quantity;
+        cart.bill = cart.items.reduce((acc, curr) => {
+          return acc + curr.quantity * curr.price;
+        }, 0);
+        cart.items[itemIndex] = product;
+        await cart.save();
+        res.status(200).send(cart);
+      } else {
+        cart.items.push({ itemId, name, quantity, price });
+        cart.bill = cart.items.reduce((acc, curr) => {
+          return acc + curr.quantity * curr.price;
+        }, 0);
+        await cart.save();
+        res.status(200).send(cart);
+      }
+    } else {
+      //no cart exists, create one
+      const newCart = await Cart_model.create({
+        owner,
+        items: [{ itemId, name, quantity, price }],
+        bill: quantity * price,
+      });
+      return res.status(201).send(newCart);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
 });
 
 // delete cart
 
 router.delete("/cart/", async (req, res) => {
-const owner = req.user._id;
-const itemId = req.query.itemId;
-try {
-   let cart = await Cart_model.findOne({ owner });
-   const itemIndex = cart.items.findIndex((item) => item.itemId == itemId);
-if (itemIndex > -1) {
-     let item = cart.items[itemIndex];
-     cart.bill -= item.quantity * item.price;
-if(cart.bill < 0) {
-      cart.bill = 0
-}
-     cart.items.splice(itemIndex, 1);
-     cart.bill = cart.items.reduce((acc, curr) => {
-return acc + curr.quantity * curr.price;
-},0)
-    cart = await cart.save();
-    res.status(200).send(cart);
-} else {
-    res.status(404).send("item not found");
-}
-} catch (error) {
-   console.log(error);
-   res.status(400).send();
-}
+  const owner = req.user._id;
+  const itemId = req.query.itemId;
+  try {
+    let cart = await Cart_model.findOne({ owner });
+    const itemIndex = cart.items.findIndex((item) => item.itemId == itemId);
+    if (itemIndex > -1) {
+      let item = cart.items[itemIndex];
+      cart.bill -= item.quantity * item.price;
+      if (cart.bill < 0) {
+        cart.bill = 0;
+      }
+      cart.items.splice(itemIndex, 1);
+      cart.bill = cart.items.reduce((acc, curr) => {
+        return acc + curr.quantity * curr.price;
+      }, 0);
+      cart = await cart.save();
+      res.status(200).send(cart);
+    } else {
+      res.status(404).send("item not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send();
+  }
 });
 
-// get orders 
+// get orders
 
-router.get('/orders', async (req, res) => {
-const owner = req.user._id;
-try {
-const order = await Order_model.find({ owner: owner }).sort({ date: -1 });
-res.status(200).send(order)
-} catch (error) {
-res.status(500).send()
-}
-})
+router.get("/orders", async (req, res) => {
+  const owner = req.user._id;
+  try {
+    const order = await Order_model.find({ owner: owner }).sort({ date: -1 });
+    res.status(200).send(order);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 // create a new item
 
-router.post('/items', async(req, res) => {
+router.post("/items", async (req, res) => {
+  const { name, description, category, price } = req.body;
   try {
-  const newItem = new Item({
-      ...req.body,
-      owner: req.user._id
-  })
-     await newItem.save()
-     res.status(201).send(newItem)
+    const newItem = new Item_model({
+      name,
+      description,
+      category,
+      price,
+    })({ name, description, category, price }).then((item) => {
+      res.status(201).json({ message: "item successfully created", item });
+    });
+    await newItem.save();
+    res.status(201).send(newItem);
   } catch (error) {
-  res.status(400).send({message: "error"})
+    res.status(400).send({ message: "error" });
   }
-  })
+});
 
- 
-
-  // fetch an item
-  router.get('/items/:id', async(req, res) => {
-    try{
-       const item = await Item_model.findOne({_id: req.params.id})
-    if(!item) {
-       res.status(404).send({error: "Item not found"})
+// fetch an item
+router.get("/items/:id", async (req, res) => {
+  try {
+    const item = await Item_model.findOne({ _id: req.params.id });
+    if (!item) {
+      res.status(404).send({ error: "Item not found" });
     }
-       res.status(200).send(item)
-    } catch (error) {
-       res.status(400).send(error)
+    res.status(200).send(item);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// fetch all items
+
+router.get("/items", async (req, res) => {
+  try {
+    const items = await Item_model.find({});
+    res.status(200).send(items);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// update an item
+router.patch("/items/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "description", "category", "price"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "invalid updates" });
+  }
+  try {
+    const item = await Item_model.findOne({ _id: req.params.id });
+    if (!item) {
+      return res.status(404).send();
     }
-    })
+    updates.forEach((update) => (item[update] = req.body[update]));
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-    // fetch all items
-
-    router.get('/items', async(req, res) => {
-      try{
-          const items = await Item_model.find({})
-          res.status(200).send(items)
-      } catch (error) {
-        res.status(400).send(error)  
-      }
-    })
-    
-    // update an item
-    router.patch('/items/:id',  async(req, res) => {
-      const updates = Object.keys(req.body)
-      const allowedUpdates = ['name', 'description', 'category', 'price']
-      const isValidOperation = updates.every((update) =>              allowedUpdates.includes(update))
-         if(!isValidOperation) {
-           return res.status(400).send({ error: 'invalid updates'})
-      }
-      try {
-        const item = await Item_model.findOne({ _id: req.params.id})
-        if(!item){
-            return res.status(404).send()
-        }
-        updates.forEach((update) => item[update] = req.body[update])
-        await item.save()
-        res.send(item)
-      } catch (error) {
-      res.status(400).send(error)
-      }
-      })
-
-      // delete an item
-      router.delete('/items/:id', async(req, res) => {
-        try {
-        const deletedItem = await Item.findOneAndDelete( {_id: req.params.id} )
-           if(!deletedItem) {
-            res.status(404).send({error: "Item not found"})
-        }
-           res.send(deletedItem)
-        } catch (error) {
-           res.status(400).send(error)
-        }
-        })
+// delete an item
+router.delete("/items/:id", async (req, res) => {
+  try {
+    const deletedItem = await Item.findOneAndDelete({ _id: req.params.id });
+    if (!deletedItem) {
+      res.status(404).send({ error: "Item not found" });
+    }
+    res.send(deletedItem);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 // auth register
 
